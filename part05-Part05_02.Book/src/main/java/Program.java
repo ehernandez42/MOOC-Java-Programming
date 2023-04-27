c2362c27e5d5446c2d5afd294f9083804bdc83e1
Eleazar Hernandez <80542211+ehernandez42@users.noreply.github.com>
2023-04-27 11:37:04 -0500

    Add files via upload

---
+++ part05-Part05_02.Book/src/main/java/Program.java
@@ -0,0 +1,15 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        // Examples I used for testing from current books on my nightstand:
+        
+        Book trev = new Book("Trevor Noah", "Born a Crime", 282);
+        System.out.println(trev);
+
+        Book chef = new Book("Kwame Onwuachi", "Notes from a Young Black Chef", 288);
+        System.out.println(chef);
+
+    }
+}
