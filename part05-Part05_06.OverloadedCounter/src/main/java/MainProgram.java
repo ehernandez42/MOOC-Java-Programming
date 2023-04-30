4c8aed1061568285a15abfa038ba2ddaa4a99296
Eleazar Hernandez <80542211+ehernandez42@users.noreply.github.com>
2023-04-30 13:22:06 -0500

    Update MainProgram.java

--- part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,6 +3,9 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Counter counter = new Counter(1);
+        
+        //this decrease call was a test to see if the negative numbers could affect the 
+        //count, which was one of the points of this exercises
         counter.decrease(-20);
         System.out.println(counter.getValue());
     }
