1d3e8fb9315b66a8a098960cb9ce7408ab727e0d
Eleazar Hernandez <80542211+ehernandez42@users.noreply.github.com>
2023-05-24 10:55:59 -0500

    Add files via upload

---
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -0,0 +1,42 @@
+
+import java.util.ArrayList;
+
+public class SimpleCollection {
+
+    private String name;
+    private ArrayList<String> elements;
+
+    public SimpleCollection(String name) {
+        this.name = name;
+        this.elements = new ArrayList<>();
+    }
+
+    public void add(String element) {
+        this.elements.add(element);
+    }
+
+    public ArrayList<String> getElements() {
+        return this.elements;
+    }
+
+    public String toString() {
+        String printOutput = "The collection " + name + " ";
+
+        if (elements.isEmpty()) {
+            return printOutput + "is empty.";
+        }
+
+        String elementInCollection = "";
+
+        for (String element : elements) {
+            elementInCollection = elementInCollection + "\n" +  element;
+         }
+
+        if (elements.size() == 1) {
+            return printOutput + "has " + elements.size() + " element:" + elementInCollection;
+        }
+
+        return printOutput + "has " + elements.size() + " elements:" + elementInCollection;
+    }
+    
+}
