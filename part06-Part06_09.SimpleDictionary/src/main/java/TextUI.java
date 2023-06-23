ccb7083fc9342e5445c9c0a3e17fe072986a46a6
Eleazar Hernandez <80542211+ehernandez42@users.noreply.github.com>
2023-06-23 11:52:40 -0500

    Add files via upload

---
+++ part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class TextUI {
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+
+
+    public TextUI (Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+
+    public void start() {
+        while(true) {
+            System.out.print("Command: ");
+            String command = scanner.nextLine();
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            }
+
+            if (command.equals("add")) {
+                System.out.print("Word: ");
+                String word = scanner.nextLine();
+                System.out.print("Translation: ");
+                String translation = scanner.nextLine();
+                dictionary.add(word, translation);
+            }
+
+            if (command.equals("search")) {
+                System.out.print("To be translated: ");
+                String queue = scanner.nextLine();
+                String translated = dictionary.translate(queue);
+                if (translated == null) {
+                    System.out.println("Word " + queue + " was not found");
+                } else {
+                    System.out.println("Translation: " + dictionary.translate(queue));
+                }
+            }
+
+            System.out.println("Unknown command");
+        }
+
+    }
+}
